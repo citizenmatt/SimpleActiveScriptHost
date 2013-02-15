bfca9f25c11b3a07e40237ab1291dae8f57a1ffd
Matt Ellis <m.t.ellis@gmail.com>
2013-02-15 21:37:58 +0000

    Initial version
    No functionality. Simple project, compiled to .net 3.5 using vs90
    toolset

---
+++ src/SimpleActiveScriptHost/ScriptHost.h
@@ -0,0 +1,13 @@
+// SimpleActiveScriptHost.h
+
+#pragma once
+
+using namespace System;
+
+namespace SimpleActiveScriptHost {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
---
+++ src/SimpleActiveScriptHost/Stdafx.cpp
@@ -0,0 +1,5 @@
+// stdafx.cpp : source file that includes just the standard includes
+// SimpleActiveScriptHost.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
