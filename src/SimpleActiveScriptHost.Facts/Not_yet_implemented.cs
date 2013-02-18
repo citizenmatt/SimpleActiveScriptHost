c8237ac2f81e8a4dbb085c4d15d21074d7724165
Matt Ellis <m.t.ellis@gmail.com>
2013-02-18 15:37:50 +0000

    Added ignored tests for unimplemented features

---
+++ src/SimpleActiveScriptHost.Facts/Not_yet_implemented.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace CitizenMatt.SimpleActiveScriptHost.Facts
+{
+    public class Not_yet_implemented
+    {
+        [Fact(Skip = "Not yet implemented")]
+        public void Should_handle_parameters_in_method_calls()
+        {
+        }
+
+        [Fact(Skip = "Not yet implemented")]
+        public void Should_handle_evaluating_expressions()
+        {
+            // Can just write a funtion and return the value, but that
+            // gets added into the codebase
+        }
+    }
+}
